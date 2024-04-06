c69000773186967c0edc65899e84054773781a88
Felipe Vilarinho <fvilarinho@gmail.com>
2024-04-06 16:04:38 -0300

    Bug fixes and code improvements.

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Environment variables.
 JAVA_CMD=$(which java)
 
+# Start command.
 $JAVA_CMD -javaagent:"$LIBS_DIR"/opentelemetry-javaagent.jar \
           -jar "$LIBS_DIR"/phonebook.jar
