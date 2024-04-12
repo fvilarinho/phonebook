177ee1b91fa63f01888d6f2839843c98cdce940f
fvilarin <fvilarin@akamai.com>
2024-04-12 13:47:20 -0300

    Code improvements.

--- iac/linode-firewall.tf
+++ iac/linode-firewall.tf
@@ -10,14 +10,36 @@ resource "linode_firewall" "default" {
   outbound_policy = "ACCEPT"
   linodes         = [ linode_instance.default.id ]
 
+  inbound {
+    label    = "allow_myip_tcp"
+    protocol = "TCP"
+    ipv4     = [ "${jsondecode(data.http.myIp.response_body).ip}/32" ]
+    action   = "ACCEPT"
+  }
+
+  inbound {
+    label    = "allow_myip_udp"
+    protocol = "UDP"
+    ipv4     = [ "${jsondecode(data.http.myIp.response_body).ip}/32" ]
+    action   = "ACCEPT"
+  }
+
+  inbound {
+    label    = "allow_myip_icmp"
+    protocol = "ICMP"
+    ipv4     = [ "${jsondecode(data.http.myIp.response_body).ip}/32" ]
+    action   = "ACCEPT"
+  }
+
   # Allows Prometheus & Jaeger external traffic only from Grafana and my IP.
   inbound {
     label    = "allow_grafana"
     protocol = "TCP"
     ports    = "9090,16686"
-    ipv4     = concat(local.settings.allowedIps, ["${jsondecode(data.http.myIp.response_body).ip}/32"])
+    ipv4     = local.settings.allowedIps
     action   = "ACCEPT"
   }
+
 
   # Allows HTTP/HTTPs external traffic from everywhere.
   inbound {
@@ -25,15 +47,6 @@ resource "linode_firewall" "default" {
     protocol = "TCP"
     ports    = "80,443"
     ipv4     = [ "0.0.0.0/0" ]
-    action   = "ACCEPT"
-  }
-
-  # Allows SSH external traffic only from my IP.
-  inbound {
-    label    = "allow_ssh"
-    protocol = "TCP"
-    ports    = "22"
-    ipv4     = [ "${jsondecode(data.http.myIp.response_body).ip}/32" ]
     action   = "ACCEPT"
   }
 
