e1589d40662ba32f3032376a829ce739e27a4905
Felipe Vilarinho <fvilarin@akamai.com>
2025-03-07 13:46:44 -0300

    Testing code analysis.

--- codeAnalysis.sh
+++ codeAnalysis.sh
@@ -38,16 +38,16 @@ function prepareToExecute() {
 function codeAnalysis() {
   ./gradlew sonar || exit 1
 
-  QUALITY_GATE_STATUS=$($CURL_CMD -s \
-                                  -u "$SONAR_USER:$SONAR_PASSWORD" \
-                                  "$SONAR_URL"/api/qualitygates/project_status?projectKey="$SONAR_PROJECT_KEY" | $JQ_CMD -r '.projectStatus.status')
+  echo "$CURL_CMD -s -u \"$SONAR_USER:$SONAR_PASSWORD\" \"$SONAR_URL\"/api/qualitygates/project_status?projectKey=\"$SONAR_PROJECT_KEY\""
 
-  if [ "$QUALITY_GATE_STATUS" == "ERROR" ]; then
-    echo
-    echo "Code analysis failed! Please check your quality gates!"
+  #| $JQ_CMD -r '.projectStatus.status')
 
-    exit 1
-  fi
+  #if [ "$QUALITY_GATE_STATUS" == "ERROR" ]; then
+  #  echo
+  #  echo "Code analysis failed! Please check your quality gates!"
+
+  #  exit 1
+  #fi
 
   $SNYK_CMD code test --severity-threshold=high
 }
