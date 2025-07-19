5ac0912382fe30497db8e672cccd4512f311a03c
Felipe Vilarinho <fvilarinho@users.noreply.github.com>
2025-07-19 19:51:35 -0300

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -38,9 +38,6 @@ function prepareToExecute() {
   export WORK_DIR="$PWD"
   export BUILD_DEFINITIONS_FILENAME="$WORK_DIR/.env"
   export PROVISIONING_DEFINITIONS_FILENAME="$WORK_DIR/iac/terraform.tfvars"
-  export PROVISIONING_STATE_CREDENTIALS_FILENAME=~/.aws/credentials
-  export SSH_PRIVATE_KEY_FILENAME=~/.ssh/id_rsa
-  export SSH_PUBLIC_KEY_FILENAME=~/.ssh/id_rsa.pub
 
   # Environment variables.
   if [ -f "$BUILD_DEFINITIONS_FILENAME" ]; then
@@ -57,29 +54,14 @@ function prepareToExecute() {
     echo "$PROVISIONING_DEFINITIONS" > "$PROVISIONING_DEFINITIONS_FILENAME"
   fi
 
-  if [ -n "$PROVISIONING_STATE_CREDENTIALS" ]; then
-    mkdir -p ~/.aws/; echo "$PROVISIONING_STATE_CREDENTIALS" > "$PROVISIONING_STATE_CREDENTIALS_FILENAME"
-  fi
-
-  if [ -n "$SSH_PRIVATE_KEY" ]; then
-    mkdir -p ~/.ssh
-
-    echo "$SSH_PRIVATE_KEY" > "$SSH_PRIVATE_KEY_FILENAME"
-
-    if [ -n "$SSH_PUBLIC_KEY" ]; then
-      echo "$SSH_PUBLIC_KEY" > "$SSH_PUBLIC_KEY_FILENAME"
-    fi
-  fi
-
   # Required binaries.
-  export CERTBOT_CMD=$(which certbot)
-  export HTPASSWD_CMD=$(which htpasswd)
-  export CURL_CMD=$(which curl)
-  export JQ_CMD=$(which jq)
-  export JAVA_CMD=$(which java)
-  export SNYK_CMD=$(which snyk)
-  export DOCKER_CMD=$(which docker)
-  export TERRAFORM_CMD=$(which terraform)
+  export HTPASSWD_CMD=$(which htpasswd 2>/dev/null)
+  export CURL_CMD=$(which curl 2>/dev/null)
+  export JQ_CMD=$(which jq 2>/dev/null)
+  export JAVA_CMD=$(which java 2>/dev/null)
+  export SNYK_CMD=$(which snyk 2>/dev/null)
+  export DOCKER_CMD=$(which docker 2>/dev/null)
+  export TERRAFORM_CMD=$(which terraform 2>/dev/null)
 }
 
 prepareToExecute
