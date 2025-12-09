79b50cfdc20eb45bbc67cdf42b24d9516c4d095f
Felipe Vilarinho <fvilarinho@gmail.com>
2025-12-09 09:17:46 -0300

    Code improvements in the pipeline.yml

--- package.sh
+++ package.sh
@@ -18,9 +18,9 @@ function prepareToExecute() {
 
 # Creates the container images.
 function package() {
-  $DOCKER_CMD buildx build \
-                     --platform linux/amd64,linux/arm64 \
-                     --tag "$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_ID/$APP_NAME:$BUILD_VERSION" .
+  $DOCKER_CMD build \
+              --platform linux/amd64,linux/arm64 \
+              --tag "$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_ID/$APP_NAME:$BUILD_VERSION" .
 
   mkdir -p build/packages || exit 1
 
