9ede72f087f6ef898456d47824bbeb8ef4879a92
Felipe Vilarinho <vilarf@amazon.com>
2026-09-07 16:44:48 -0300

    Adding terraform provisioning to AWS.

--- iac/subnet.tf
+++ iac/subnet.tf
@@ -1,19 +1,49 @@
-# Creates the VPC subnets.
-resource "aws_subnet" "phonebook_pvt_subnet" {
+resource "aws_subnet" "phonebook_pub_a" {
   vpc_id                                      = aws_vpc.phonebook.id
   cidr_block                                  = "10.0.1.0/24"
-  enable_resource_name_dns_a_record_on_launch = true
   availability_zone                           = "us-east-1a"
+  map_public_ip_on_launch                     = true
+  enable_resource_name_dns_a_record_on_launch = true
 
-  depends_on = [aws_vpc.phonebook]
+  tags = {
+    Name                     = "phonebook-pub-subnet-a"
+    "kubernetes.io/role/elb" = "1"
+  }
 }
 
-resource "aws_subnet" "phonebook_pub_subnet" {
+resource "aws_subnet" "phonebook_pvt_a" {
   vpc_id                                      = aws_vpc.phonebook.id
   cidr_block                                  = "10.0.2.0/24"
-  enable_resource_name_dns_a_record_on_launch = true
-  map_public_ip_on_launch                     = true
   availability_zone                           = "us-east-1a"
+  enable_resource_name_dns_a_record_on_launch = true
 
-  depends_on = [aws_vpc.phonebook]
+  tags = {
+    Name                              = "phonebook-pvt-subnet-a"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_subnet" "phonebook_pub_b" {
+  vpc_id                                      = aws_vpc.phonebook.id
+  cidr_block                                  = "10.0.3.0/24"
+  availability_zone                           = "us-east-1b"
+  map_public_ip_on_launch                     = true
+  enable_resource_name_dns_a_record_on_launch = true
+
+  tags = {
+    Name                     = "phonebook-pub-subnet-b"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "phonebook_pvt_b" {
+  vpc_id                                      = aws_vpc.phonebook.id
+  cidr_block                                  = "10.0.4.0/24"
+  availability_zone                           = "us-east-1b"
+  enable_resource_name_dns_a_record_on_launch = true
+
+  tags = {
+    Name                              = "phonebook-pvt-subnet-b"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
 }
