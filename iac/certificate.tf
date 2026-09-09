141b625c66f484de55099bfea59b6e4b86f0ded2
Felipe Vilarinho <vilarf@amazon.com>
2026-09-09 18:31:37 -0300

    Remove deprecated files.

--- iac/certificate.tf
+++ iac/certificate.tf
@@ -1,15 +1,9 @@
-data "cloudflare_zone" "phonebook" {
-  filter = {
-    name = local.secrets.frontend.domain
-  }
-}
-
 resource "tls_private_key" "phonebook" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_acm_certificate" "phonebook_cluster" {
+resource "aws_acm_certificate" "phonebook" {
   domain_name       = "${local.secrets.frontend.host}.${local.secrets.frontend.domain}"
   validation_method = "DNS"
 
@@ -18,50 +12,15 @@ resource "aws_acm_certificate" "phonebook_cluster" {
   }
 }
 
-resource "cloudflare_dns_record" "phonebook_certificate_validation" {
-  for_each = {
-    for option in aws_acm_certificate.phonebook_cluster.domain_validation_options :
-
-    option.domain_name => {
-      name    = trimsuffix(option.resource_record_name, ".")
-      type    = option.resource_record_type
-      content = option.resource_record_value
-    }
-  }
-
-  zone_id = data.cloudflare_zone.phonebook.id
-  name    = each.value.name
-  type    = each.value.type
-  content = each.value.content
-  ttl     = 60
-  proxied = false
-
-  depends_on = [aws_acm_certificate.phonebook_cluster]
-}
-
-resource "aws_acm_certificate_validation" "phonebook_cluster" {
-  certificate_arn = aws_acm_certificate.phonebook_cluster.arn
+resource "aws_acm_certificate_validation" "phonebook" {
+  certificate_arn = aws_acm_certificate.phonebook.arn
 
   validation_record_fqdns = [
-    for option in aws_acm_certificate.phonebook_cluster.domain_validation_options : option.resource_record_name
+    for option in aws_acm_certificate.phonebook.domain_validation_options : option.resource_record_name
   ]
 
   depends_on = [
-    aws_acm_certificate.phonebook_cluster,
+    aws_acm_certificate.phonebook,
     cloudflare_dns_record.phonebook_certificate_validation
-  ]
-}
-
-resource "cloudflare_dns_record" "phonebook_cluster" {
-  zone_id = data.cloudflare_zone.phonebook.id
-  name    = "${local.frontend.host}.${local.secrets.frontend.domain}"
-  type    = "CNAME"
-  content = aws_lb.phonebook_cluster.dns_name
-  ttl     = 60
-  proxied = false
-
-  depends_on = [
-    data.cloudflare_zone.phonebook,
-    aws_lb.phonebook_cluster
   ]
 }
