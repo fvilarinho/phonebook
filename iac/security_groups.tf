80e0a27e87e02df11b4570b891288b077b01facf
Felipe Vilarinho <vilarf@amazon.com>
2026-09-09 15:39:34 -0300

    Code improvements

--- iac/security_groups.tf
+++ iac/security_groups.tf
@@ -6,8 +6,33 @@ locals {
   my_ip = jsondecode(chomp(data.http.myip.response_body)).ip
 }
 
+resource "aws_security_group" "phonebook_cluster_bastion_pub_traffic" {
+  name        = "${local.build.name}-cluster-bastion-pub-traffic"
+  description = "Allow public traffic to phonebook cluster bastion."
+  vpc_id      = aws_vpc.phonebook.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${local.my_ip}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  depends_on = [
+    aws_vpc.phonebook,
+    data.http.myip
+  ]
+}
+
 resource "aws_security_group" "phonebook_cluster_lb_traffic" {
-  name        = "${var.settings.general.name}-cluster-lb-traffic"
+  name        = "${local.build.name}-cluster-lb-traffic"
   description = "Allow public HTTP and HTTPS traffic to the K3s application ALB."
   vpc_id      = aws_vpc.phonebook.id
 
@@ -35,8 +60,9 @@ resource "aws_security_group" "phonebook_cluster_lb_traffic" {
   depends_on = [aws_vpc.phonebook]
 }
 
+
 resource "aws_security_group" "phonebook_database_pub_traffic" {
-  name        = "${var.settings.general.name}-database-pub-traffic"
+  name        = "${local.build.name}-database-pub-traffic"
   description = "Allow public traffic to phonebook database."
   vpc_id      = aws_vpc.phonebook.id
 
@@ -61,7 +87,7 @@ resource "aws_security_group" "phonebook_database_pub_traffic" {
 }
 
 resource "aws_security_group" "phonebook_database_pvt_traffic" {
-  name        = "${var.settings.general.name}-database-pvt-traffic"
+  name        = "${local.build.name}-database-pvt-traffic"
   description = "Allow private traffic to phonebook database."
   vpc_id      = aws_vpc.phonebook.id
 
@@ -69,7 +95,7 @@ resource "aws_security_group" "phonebook_database_pvt_traffic" {
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
-    cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}"]
+    cidr_blocks = ["${aws_instance.phonebook_cluster_workernode1.private_ip}/32", "${aws_instance.phonebook_cluster_workernode2.private_ip}/32"]
   }
 
   egress {
@@ -81,14 +107,14 @@ resource "aws_security_group" "phonebook_database_pvt_traffic" {
 
   depends_on = [
     aws_vpc.phonebook,
-    aws_subnet.phonebook_pvt_a,
-    aws_subnet.phonebook_pvt_b
+    aws_instance.phonebook_cluster_workernode1,
+    aws_instance.phonebook_cluster_workernode2
   ]
 }
 
 # Allows only the database EC2 to initiate SSH connections to private workers.
 resource "aws_security_group" "phonebook_cluster_workernodes_traffic" {
-  name        = "${var.settings.general.name}-cluster-workernodes-traffic"
+  name        = "${local.build.name}-cluster-workernodes-traffic"
   description = "Allow traffic to workernodes"
   vpc_id      = aws_vpc.phonebook.id
 
@@ -96,35 +122,7 @@ resource "aws_security_group" "phonebook_cluster_workernodes_traffic" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}", "${aws_subnet.phonebook_pub_a.cidr_block}", "${aws_subnet.phonebook_pub_b.cidr_block}"]
-  }
-
-  ingress {
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}", "${aws_subnet.phonebook_pub_a.cidr_block}", "${aws_subnet.phonebook_pub_b.cidr_block}"]
-  }
-
-  ingress {
-    from_port   = 9345
-    to_port     = 9345
-    protocol    = "tcp"
-    cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}", "${aws_subnet.phonebook_pub_a.cidr_block}", "${aws_subnet.phonebook_pub_b.cidr_block}"]
-  }
-
-  ingress {
-    from_port   = 10250
-    to_port     = 10250
-    protocol    = "tcp"
-    cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}", "${aws_subnet.phonebook_pub_a.cidr_block}", "${aws_subnet.phonebook_pub_b.cidr_block}"]
-  }
-
-  ingress {
-    from_port   = 8472
-    to_port     = 8472
-    protocol    = "udp"
-    cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}", "${aws_subnet.phonebook_pub_a.cidr_block}", "${aws_subnet.phonebook_pub_b.cidr_block}"]
+    cidr_blocks = ["${aws_instance.phonebook_cluster_bastion.public_ip}/32", "${aws_instance.phonebook_cluster_bastion.private_ip}/32"]
   }
 
   ingress {
@@ -141,6 +139,34 @@ resource "aws_security_group" "phonebook_cluster_workernodes_traffic" {
     cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}", "${aws_subnet.phonebook_pub_a.cidr_block}", "${aws_subnet.phonebook_pub_b.cidr_block}"]
   }
 
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}","${aws_instance.phonebook_cluster_bastion.public_ip}/32", "${aws_instance.phonebook_cluster_bastion.private_ip}/32"]
+  }
+
+  ingress {
+    from_port   = 9345
+    to_port     = 9345
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}"]
+  }
+
+  ingress {
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}"]
+  }
+
+  ingress {
+    from_port   = 8472
+    to_port     = 8472
+    protocol    = "udp"
+    cidr_blocks = ["${aws_subnet.phonebook_pvt_a.cidr_block}", "${aws_subnet.phonebook_pvt_b.cidr_block}"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -154,7 +180,6 @@ resource "aws_security_group" "phonebook_cluster_workernodes_traffic" {
     aws_subnet.phonebook_pvt_b,
     aws_subnet.phonebook_pub_a,
     aws_subnet.phonebook_pub_b,
-    aws_security_group.phonebook_database_pub_traffic,
-    aws_security_group.phonebook_cluster_lb_traffic
+    aws_instance.phonebook_cluster_bastion
   ]
 }
