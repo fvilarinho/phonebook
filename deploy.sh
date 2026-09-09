a834a4acd42bd178cade62bfe07764764c10209b
Felipe Vilarinho <vilarf@amazon.com>
2026-09-09 18:31:01 -0300

    Remove deprecated files.

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,6 @@ function checkDependencies() {
 
         exit 1
     fi
-
-    if [ -z "${AWS_REGION:-}" ]; then
-        echo "AWS_REGION must be set in .secrets before deployment."
-
-        exit 1
-    fi
 }
 
 # Prepares the environment to run this script.
@@ -27,8 +21,8 @@ function deploy() {
     $TERRAFORM_CMD init -migrate-state \
                         -upgrade \
                         -force-copy \
-                        -backend-config="bucket=$TERRAFORM_S3_BACKEND_STATE_BUCKET" \
-                        -backend-config="key=$TERRAFORM_S3_BACKEND_STATE_KEY" \
+                        -backend-config="bucket=$TERRAFORM_STATE_BUCKET" \
+                        -backend-config="key=$TERRAFORM_STATE_KEY" \
                         -backend-config="region=$AWS_REGION" || exit 1
 
     $TERRAFORM_CMD plan -out="$TMPDIR"/phonebook.tfplan || exit 1
