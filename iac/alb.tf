341625279e4135c87ca072c67b4f22056fe8a669
Felipe Vilarinho <vilarf@amazon.com>
2026-09-09 18:31:46 -0300

    Remove deprecated files.

--- iac/alb.tf
+++ iac/alb.tf
@@ -73,7 +73,7 @@ resource "aws_lb_listener" "secure_phonebook_cluster" {
   load_balancer_arn = aws_lb.phonebook_cluster.arn
   port              = 443
   protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate_validation.phonebook_cluster.certificate_arn
+  certificate_arn   = aws_acm_certificate_validation.phonebook.certificate_arn
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
 
   default_action {
@@ -83,7 +83,7 @@ resource "aws_lb_listener" "secure_phonebook_cluster" {
 
   depends_on = [
     aws_lb.phonebook_cluster,
-    aws_acm_certificate_validation.phonebook_cluster,
+    aws_acm_certificate_validation.phonebook,
     aws_lb_target_group.phonebook_cluster
   ]
 }
