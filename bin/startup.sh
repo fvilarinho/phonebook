7442db8773e497fcfef8c401ec3fb5c75a15101f
Felipe Vilarinho <vilarf@amazon.com>
2026-09-09 15:52:04 -0300

    Bug fixes.

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env /bash
+#!/usr/bin/env bash
 
 # Show banner.
 if [ -e "$ETC_DIR/banner.txt" ]; then
@@ -18,7 +18,7 @@ if [ "$OBSERVABILITY_ENABLED" == true ]; then
   STARTUP_OPTS="$STARTUP_OPTS -Dlogging.config=file:$ETC_DIR/logback.xml"
 fi
 
-STARTUP_CMD="$JAVA_CMD $STARTUP_OPTS -jar $LIBS_DIR/phonebook.jar"
+STARTUP_CMD="$JAVA_CMD $STARTUP_OPTS -jar $LIBS_DIR/$BUILD_NAME.jar"
 
 # Execute the startup script.
 eval "$STARTUP_CMD"
