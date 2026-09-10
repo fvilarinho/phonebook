ae7e598d177d038936c02a38883b0278237ac107
Felipe Vilarinho <vilarf@amazon.com>
2026-09-10 12:48:47 -0300

    Code improvements.

--- iac/database.tf
+++ iac/database.tf
@@ -1,26 +1,27 @@
 locals {
-  phonebook_database_environment_filename   = ".env"
-  phonebook_database_backup_script_filename = abspath(pathexpand("../bin/backup.sh"))
-  phonebook_database_start_script_filename  = abspath(pathexpand("../start.sh"))
-  phonebook_database_stop_script_filename   = abspath(pathexpand("../stop.sh"))
-  phonebook_database_helper_script_filename = abspath(pathexpand("../functions.sh"))
+  database_environment_filename   = ".env"
+  database_backup_script_filename = abspath(pathexpand("../bin/backup.sh"))
+  database_backup_cron_filename   = abspath(pathexpand("../etc/backup.cron"))
+  database_start_script_filename  = abspath(pathexpand("../start.sh"))
+  database_stop_script_filename   = abspath(pathexpand("../stop.sh"))
+  database_helper_script_filename = abspath(pathexpand("../functions.sh"))
 }
 
 locals {
-  phonebook_database_manifest_filename             = "docker-compose.yaml"
-  phonebook_database_settings_manifest_filename    = "database-settings.yaml"
-  phonebook_database_credentials_manifest_filename = "database-credentials.yaml"
-  phonebook_database_setup_hash                    = "${aws_instance.phonebook_database.id}-${filemd5(local.phonebook_database_backup_script_filename)}-${md5(local.phonebook_database_environment)}-${md5(local.phonebook_database_manifest)}-${filemd5(local.banner_filename)}-${filemd5(local.phonebook_database_start_script_filename)}-${filemd5(local.phonebook_database_stop_script_filename)}-${filemd5(local.phonebook_database_helper_script_filename)}}"
+  database_manifest_filename             = "docker-compose.yaml"
+  database_settings_manifest_filename    = "database-settings.yaml"
+  database_credentials_manifest_filename = "database-credentials.yaml"
+  database_setup_hash                    = "${aws_instance.phonebook_database.id}-${filemd5(local.database_backup_script_filename)}-${filemd5(local.database_backup_cron_filename)}-${md5(local.database_environment)}-${md5(local.database_manifest)}-${filemd5(local.banner_filename)}-${filemd5(local.database_start_script_filename)}-${filemd5(local.database_stop_script_filename)}-${filemd5(local.database_helper_script_filename)}}"
 }
 
 locals {
-  phonebook_database_environment = <<EOT
+  database_environment = <<-EOT
 export DB_USER=${local.secrets.database.user}
 export DB_PASSWORD=${local.secrets.database.password}
 export BACKUP_BUCKET="${local.build.name}-backup"
 EOT
 
-  phonebook_database_settings_manifest = <<EOT
+  database_settings_manifest = <<-EOT
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -31,7 +32,7 @@ data:
   DB_NAME: ${local.secrets.database.name}
 EOT
 
-  phonebook_database_credentials_manifest = <<EOT
+  database_credentials_manifest = <<-EOT
 apiVersion: v1
 kind: Secret
 metadata:
@@ -43,7 +44,7 @@ stringData:
   DB_PASSWORD: ${local.secrets.database.password}
 EOT
 
-  phonebook_database_manifest = <<EOT
+  database_manifest = <<-EOT
 services:
   database:
     image: mongo:7.0
@@ -65,48 +66,53 @@ EOT
 
 resource "null_resource" "phonebook_database_setup" {
   triggers = {
-    hash = local.phonebook_database_setup_hash
+    hash = local.database_setup_hash
   }
 
   connection {
     host        = aws_instance.phonebook_database.public_ip
-    user        = local.user
+    user        = local.compute.user
     private_key = tls_private_key.phonebook.private_key_pem
   }
 
   provisioner "file" {
-    source      = local.phonebook_database_backup_script_filename
-    destination = "${local.homeDir}/${basename(local.phonebook_database_backup_script_filename)}"
+    source      = local.database_backup_script_filename
+    destination = "${local.compute.home_dir}/${basename(local.database_backup_script_filename)}"
   }
 
   provisioner "file" {
-    content     = local.phonebook_database_environment
-    destination = "${local.homeDir}/${local.phonebook_database_environment_filename}"
+    source      = local.database_backup_cron_filename
+    destination = "${local.compute.home_dir}/${basename(local.database_backup_cron_filename)}"
   }
 
   provisioner "file" {
-    content     = local.phonebook_database_manifest
-    destination = "${local.homeDir}/${local.phonebook_database_manifest_filename}"
+    content     = local.database_environment
+    destination = "${local.compute.home_dir}/${local.database_environment_filename}"
+  }
+
+  provisioner "file" {
+    content     = local.database_manifest
+    destination = "${local.compute.home_dir}/${local.database_manifest_filename}"
   }
 
   provisioner "file" {
     source      = local.banner_filename
-    destination = "${local.homeDir}/${basename(local.banner_filename)}"
+    destination = "${local.compute.home_dir}/${basename(local.banner_filename)}"
   }
 
   provisioner "file" {
-    source      = local.phonebook_database_start_script_filename
-    destination = "${local.homeDir}/${basename(local.phonebook_database_start_script_filename)}"
+    source      = local.database_start_script_filename
+    destination = "${local.compute.home_dir}/${basename(local.database_start_script_filename)}"
   }
 
   provisioner "file" {
-    source      = local.phonebook_database_stop_script_filename
-    destination = "${local.homeDir}/${basename(local.phonebook_database_stop_script_filename)}"
+    source      = local.database_stop_script_filename
+    destination = "${local.compute.home_dir}/${basename(local.database_stop_script_filename)}"
   }
 
   provisioner "file" {
-    source      = local.phonebook_database_helper_script_filename
-    destination = "${local.homeDir}/${basename(local.phonebook_database_helper_script_filename)}"
+    source      = local.database_helper_script_filename
+    destination = "${local.compute.home_dir}/${basename(local.database_helper_script_filename)}"
   }
 
   provisioner "remote-exec" {
@@ -114,11 +120,14 @@ resource "null_resource" "phonebook_database_setup" {
       "echo 'Waiting for Cloud-init to complete...'",
       "sudo cloud-init status --wait",
       "echo 'Cloud-init finished!'",
-      "cd ${local.homeDir}",
+      "cd ${local.compute.home_dir}",
       "chmod u+x *.sh",
+      "chmod 0600 .env",
       "chmod og-rwx *.sh",
       "chmod og-rwx *.txt",
       "chmod og-rwx *.yaml",
+      "sudo install -o root -g root -m 0644 backup.cron /etc/cron.d/phonebook-backup",
+      "sudo systemctl enable --now cron",
       "sudo ./start.sh"
     ]
   }
