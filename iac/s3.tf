12891b5f0f8867f4f593631d9d54e1e110dfe943
Felipe Vilarinho <me@vila.net.br>
2026-09-10 20:10:00 -0300

    Bug fixes and code improvements. Adding release pipeline. Add WAF and Logging.

--- iac/s3.tf
+++ iac/s3.tf
@@ -77,10 +77,35 @@ resource "aws_s3_bucket" "phonebook_backup" {
 
 resource "aws_s3_bucket_public_access_block" "phonebook_backup" {
   bucket                  = aws_s3_bucket.phonebook_backup.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+  depends_on = [aws_s3_bucket.phonebook_backup]
+}
+
+resource "aws_s3_bucket_policy" "phonebook_backup" {
+  bucket = aws_s3_bucket.phonebook_backup.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:ListBucket"
+        Resource  = "${aws_s3_bucket.phonebook_backup.arn}"
+      },
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.phonebook_backup.arn}/*"
+      }
+    ]
+  })
 
   depends_on = [aws_s3_bucket.phonebook_backup]
 }
