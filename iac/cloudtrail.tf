c08dd295100cf0b7e17313616d34f49f589c48f2
Felipe Vilarinho <me@vila.net.br>
2026-09-11 23:25:30 -0300

    Bug fixes and code improvements.

--- iac/cloudtrail.tf
+++ iac/cloudtrail.tf
@@ -1,10 +1,7 @@
 resource "aws_cloudtrail" "phonebook" {
-  name                          = "${local.prefix}-${local.build.name}-logs"
-  s3_bucket_name                = aws_s3_bucket.phonebook_logs.id
-  s3_key_prefix                 = "audit"
-  include_global_service_events = true
-  is_multi_region_trail         = true
-  enable_log_file_validation    = true
+  name           = "${local.prefix}-${local.build.name}-logs"
+  s3_bucket_name = aws_s3_bucket.phonebook_logs.id
+  s3_key_prefix  = "audit"
 
   depends_on = [aws_s3_bucket_policy.phonebook_logs]
 }
